ca3976c9b64859a06b389c9a79c5349f326789e4
Aman-zishan <amanzishan.az@gmail.com>
2020-06-30 11:51:22 +0530

    v1.0.0

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bmi_app/input_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(new MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BMI Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: InputPage(),
+    );
+  }
+}
